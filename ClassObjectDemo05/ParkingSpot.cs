684d0fe44686a1c46e9e69cde4e103b9d0260026
saddamhossain <saddamhossain953@gmail.com>
2024-10-22 00:49:31 +0600

    Add project files.

---
+++ ClassObjectDemo05/ParkingSpot.cs
@@ -0,0 +1,48 @@
+﻿namespace ClassObjectDemo05;
+
+public class ParkingSpot
+{
+    public int SpotNumber { get; private set; }
+    public string VehicleType { get; private set; }
+    public bool IsOccupied { get; private set; }
+
+    public ParkingSpot(int spotNumber)
+    {
+        SpotNumber = spotNumber;
+        VehicleType = string.Empty;
+        IsOccupied = false;
+    }
+
+    public bool ParkVehicle(string vehicleType)
+    {
+        if (!IsOccupied)
+        {
+            VehicleType = vehicleType;
+            IsOccupied = true;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool RemoveVehicle()
+    {
+        if (IsOccupied)
+        {
+            VehicleType = string.Empty;
+            IsOccupied = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool IsAvailable()
+    {
+        return !IsOccupied;
+    }
+}
---
+++ ClassObjectDemo05/Program.cs
@@ -0,0 +1,34 @@
+﻿ParkingSpot parkingSpot = new ParkingSpot(1);
+
+if (parkingSpot.ParkVehicle("Car"))
+{
+    Console.WriteLine($"Vehicle 'Car' parked in spot {parkingSpot.SpotNumber}.");
+}
+else
+{
+    Console.WriteLine($"Spot {parkingSpot.SpotNumber} is already occupied.");
+}
+
+
+if (!parkingSpot.ParkVehicle("Motorcycle"))
+{
+    Console.WriteLine($"Cannot park Motorcycle. Spot {parkingSpot.SpotNumber} is already occupied by a {parkingSpot.VehicleType}.");
+}
+
+
+if (parkingSpot.RemoveVehicle())
+{
+    Console.WriteLine($"Vehicle removed from spot {parkingSpot.SpotNumber}.");
+}
+
+
+if (parkingSpot.IsAvailable())
+{
+    Console.WriteLine($"Spot {parkingSpot.SpotNumber} is available for parking.");
+}
+
+
+if (parkingSpot.ParkVehicle("Truck"))
+{
+    Console.WriteLine($"Vehicle 'Truck' parked in spot {parkingSpot.SpotNumber}.");
+}
